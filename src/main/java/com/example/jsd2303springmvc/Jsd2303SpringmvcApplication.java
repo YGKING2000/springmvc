1fe946fb6e337b18ee94707c1394aa097c81a9fa
YGKING <hrd18960706057@163.com>
2023-05-08 09:46:37 +0800

    initial project

---
+++ src/main/java/com/example/jsd2303springmvc/Jsd2303SpringmvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.jsd2303springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jsd2303SpringmvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jsd2303SpringmvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jsd2303springmvc/Jsd2303SpringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsd2303springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jsd2303SpringmvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
